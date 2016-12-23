e1576b10ee4a427ba85d7c4068505b2e1239addb
Daniel Lewis <thecontrarian42@gmail.com>
2016-12-22 20:39:30 -0600

    Declare Computer structs

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,32 @@
+#[derive(Default)]
+struct CPU {
+    v: [u8; 16],
+    i: u16,
+    dt: u8,
+    st: u8,
+    pc: u16,
+    sp: u8,
+    stack: [u16; 16],
+}
+
+
+// #[derive(Default)]
+struct Computer {
+    ram: [u8; 4096],
+    cpu: CPU,
+}
+
+impl Default for Computer {
+     fn default() -> Computer {
+         Computer {
+             ram: [0u8; 4096],
+             cpu: Default::default(),
+         }
+     }
+}
+
 fn main() {
+    let mut computer : Computer = Default::default();
+    computer.cpu.pc = 0x200;
     println!("Hello, world!");
 }
