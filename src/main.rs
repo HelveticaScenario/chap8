a3d641ce6f3083dbd7ba027ae918a70390ed7626
Daniel Lewis <thecontrarian42@gmail.com>
2016-12-22 21:24:57 -0600

    File Loading

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,6 @@
+use std::fs::File;
+use std::env;
+
 #[derive(Default)]
 struct CPU {
     v: [u8; 16],
@@ -25,8 +28,23 @@ impl Default for Computer {
      }
 }
 
+impl Computer {
+
+}
+
+// cargo run -- ./games/TANK
+
 fn main() {
     let mut computer : Computer = Default::default();
     computer.cpu.pc = 0x200;
+    let mut f = File::open(env::args().nth(1).unwrap()).unwrap();
+    {
+        let mut slice = &mut computer.ram[0x200..];
+        slice[0] = 55u8;
+    }
+    println!("{:?}", computer.ram[0x200]);
+    
+
+    
     println!("Hello, world!");
 }
